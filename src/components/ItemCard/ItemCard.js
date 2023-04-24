4731a29f510b2f648fc8acf90b157128b916d3ee
admrts <alidemirtas.7@icloud.com>
2023-04-24 23:52:02 +0300

    create components

---
+++ src/components/ItemCard/ItemCard.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import styles from "./ItemCard.styles";
+
+const ItemCard = () => {
+  return (
+    <View>
+      <Text>ItemCard</Text>
+    </View>
+  );
+};
+
+export default ItemCard;
---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import styles from "./ItemList.styles";
+
+const ItemList = () => {
+  return (
+    <View>
+      <Text>ItemList</Text>
+    </View>
+  );
+};
+
+export default ItemList;
