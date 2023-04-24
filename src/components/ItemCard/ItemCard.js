27218525821a15c5eb17e4ab1f10d608316a0352
admrts <alidemirtas.7@icloud.com>
2023-04-25 01:54:57 +0300

    categories done

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,10 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import styles from "./ItemCard.styles";
+import { AntDesign } from "@expo/vector-icons";
 
-const ItemCard = () => {
+const ItemCard = ({ data }) => {
   return (
-    <View>
-      <Text>ItemCard</Text>
+    <View style={styles.container}>
+      <View style={styles.image_title_container}>
+        <Image style={styles.image} source={{ uri: data.strCategoryThumb }} />
+        <Text style={styles.text}>{data.strCategory}</Text>
+      </View>
+      <AntDesign name="rightsquareo" size={24} color="#CFC7BC" />
     </View>
   );
 };
--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,16 +1,26 @@
-import { View, Text } from "react-native";
-import { BASE_URL } from "@env";
-import { useEffect } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import ItemCard from "../../components/ItemCard/ItemCard";
+import useFetch from "../../hooks/useFetch/useFetch";
 
 const Categories = () => {
-  useEffect(() => {
-    console.log("w" + BASE_URL);
-  }, []);
+  const { data, error, loading } = useFetch(
+    "https://www.themealdb.com/api/json/v1/1/categories.php"
+  );
   return (
-    <View>
-      <Text>Categories</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.idCategory}
+        renderItem={({ item }) => <ItemCard data={item} />}
+      />
     </View>
   );
 };
 
 export default Categories;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F9F5EB",
+  },
+});
